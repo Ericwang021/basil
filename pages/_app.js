480f668cc32082a779683609af61a1a9616376ce
EricWang <ericw0221@gmail.com>
2020-07-19 14:32:45 +0800

    整理排版

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,11 @@
-import "../styles/styles.scss";
-import Layout from "../Layout/Layout";
-
+import '../styles/styles.scss';
+import Layout from '../Layout/Layout';
 const App = ({ Component, pageProps }) => {
-	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
-	);
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    );
 };
 
 export default App;
